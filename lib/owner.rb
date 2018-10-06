107a5279e7e418763948a069b8e25785caaf3891
Brian Birdwell <bbirdwell@gmail.com>
2018-10-06 20:59:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,18 +6,16 @@ class Owner
     @pets = {fishes: [], cats: [], dogs: []}
   end
   
-  def buy_cat
+  def buy_cat(name)
+    @pets[:cats] << cat.new(name)
   end
   
   def buy_dog(name)
+    @pets[:dogs] << dog.new(name)
   end
   
   def buy_fish(name)
+    @pets[:fishes] << fish.new(name)
   end
 end
 
-
-
-* When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following: 
-  * *Make a new instance of the appropriate pet, initializing it with that name*.
-  * Associate that new pet instance to the owner by adding it to the appropriate array-value of the `@pets` hash stored in the `pets` `attr_accessor`.
