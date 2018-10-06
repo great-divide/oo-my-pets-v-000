f840e516652fa24d92d678fdb6cf6e103b0d5684
Brian Birdwell <bbirdwell@gmail.com>
2018-10-06 21:45:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
   
-  attr_accessor :pets
+  attr_accessor :pets, :name
   attr_accessor  :species
   
   @@all = []
@@ -29,15 +29,15 @@ class Owner
   end
   
   def buy_cat(name)
-    @pets[:cats] << cat.new(name)
+    @pets[:cats] << Cat.new(name)
   end
   
   def buy_dog(name)
-    @pets[:dogs] << dog.new(name)
+    @pets[:dogs] << Dog.new(name)
   end
   
   def buy_fish(name)
-    @pets[:fishes] << fish.new(name)
+    @pets[:fishes] << Fish.new(name)
   end
   
   def walk_dogs
