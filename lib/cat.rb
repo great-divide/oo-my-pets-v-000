2df71355e235fbad20496a2933a21126cf4b589f
Brian Birdwell <bbirdwell@gmail.com>
2018-10-06 21:32:04 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_reader :name, :species
   attr_accessor :mood
   
-  def initalize(name)
+  def initialize(name)
     @name = name
     self.species = "cat"
     self.mood = "nervous"
